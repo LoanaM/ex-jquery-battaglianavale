f3f9ac9d776e335413ddfff9884213647c3a99c1
LoanaWitch <loanawitch@live.com>
2018-04-04 22:05:08 +0200

    jq navale finito

--- battagliamain.js
+++ battagliamain.js
@@ -1,3 +1,27 @@
+//genero 15 navi casuali diverse
+var ships = [];
+for (var i = 0; i < 15; i++) {
+  do {
+    var navecas = Math.floor((Math.random() * 64) + 1);
+  } while (ships.includes(navecas)==true)
+  ships.push(navecas)
+}
+console.log(ships)
+
+countgreen = 0;
+countred = 0;
+
+//funzione che cambia sfondo e contatore
 $('td').click(function(){
-  $(this).css('background-color','red');
+  var nave = $(this).attr('id')
+  if (ships.includes(parseInt(nave))) {
+    $(this).css('background-color','red');
+    countred++
+    $('.cred').html(countred)
+  }
+  else {
+    $(this).css('background-color','green')
+    countgreen++
+    $('.cgreen').html(countgreen)
+  }
 })
